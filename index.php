39fa9de1731cb631ffd69f974a029c7902e6110f
githiroshi <githiroshi@outlook.com>
2017-01-16 21:34:42 +0900

    位置

--- index.php
+++ index.php
@@ -37,8 +37,8 @@ foreach ($events as $event) {
 // );
 
 // replyTextMessage($bot, $event->getReplyToken(), "TextMessage");
-replyImageMessage($bot, $event->getReplyToken(), "https://" . $_SERVER["HTTP_HOST"] . "/imgs/original.jpg", "https://" . $_SERVER["HTTP_HOST"] . "/imgs/preview.jpg");
-
+// replyImageMessage($bot, $event->getReplyToken(), "https://" . $_SERVER["HTTP_HOST"] . "/imgs/original.jpg", "https://" . $_SERVER["HTTP_HOST"] . "/imgs/preview.jpg");
+replyLocationMessage($bot, $event->getReplyToken(), "LINE", "東京都渋谷区渋谷2-21-1 ヒカリエ27階", 35.659025, 139.703473);
 }
 
 function replyTextMessage($bot, $replyToken, $text) {
@@ -50,6 +50,13 @@ function replyTextMessage($bot, $replyToken, $text) {
 
 function replyImageMessage($bot, $replyToken, $originalImageUrl, $previewImageUrl) {
   $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalImageUrl, $previewImageUrl));
+  if (!$response->isSucceeded()) {
+    error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
+  }
+}
+
+function replyLocationMessage($bot, $replyToken, $title, $address, $lat, $lon) {
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder($title, $address, $lat, $lon));
   if (!$response->isSucceeded()) {
     error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
   }
