b56b489687678eddb68767ca6c2c07d380e05b9e
githiroshi <githiroshi@outlook.com>
2017-01-16 21:58:33 +0900

    スタンプ

--- index.php
+++ index.php
@@ -38,7 +38,9 @@ foreach ($events as $event) {
 
 // replyTextMessage($bot, $event->getReplyToken(), "TextMessage");
 // replyImageMessage($bot, $event->getReplyToken(), "https://" . $_SERVER["HTTP_HOST"] . "/imgs/original.jpg", "https://" . $_SERVER["HTTP_HOST"] . "/imgs/preview.jpg");
-replyLocationMessage($bot, $event->getReplyToken(), "LINE", "東京都渋谷区渋谷2-21-1 ヒカリエ27階", 35.659025, 139.703473);
+// replyLocationMessage($bot, $event->getReplyToken(), "LINE", "東京都渋谷区渋谷2-21-1 ヒカリエ27階", 35.659025, 139.703473);
+replyStickerMessage($bot, $event->getReplyToken(), 41, 2);
+
 }
 
 function replyTextMessage($bot, $replyToken, $text) {
@@ -57,6 +59,13 @@ function replyImageMessage($bot, $replyToken, $originalImageUrl, $previewImageUr
 
 function replyLocationMessage($bot, $replyToken, $title, $address, $lat, $lon) {
   $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder($title, $address, $lat, $lon));
+  if (!$response->isSucceeded()) {
+    error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
+  }
+}
+
+function replyStickerMessage($bot, $replyToken, $packageId, $stickerId) {
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder($packageId, $stickerId));
   if (!$response->isSucceeded()) {
     error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
   }
