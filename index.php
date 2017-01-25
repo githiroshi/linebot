0bcca187825ebc9c8b0e1758b12f18a1cf30e4c8
githiroshi <githiroshi@outlook.com>
2017-01-25 10:20:20 +0900

    問

--- index.php
+++ index.php
@@ -37,14 +37,14 @@ foreach ($events as $event) {
   }
   $bot->replyText($event->getReplyToken(), makeTemplate($event->getText()));
   $profile = $bot->getProfile($event->getUserId())->getJSONDecodedBody();
-$message = $profile["displayName"] . "さんの現在もバリバリのエンジニア？";
-// $bot->replyMessage($event->getReplyToken(),
-//   (new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder())
-//     ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message))
-//     ->add(new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1, 114))
-// );
+$message = $profile["displayName"] . "さんは現在もバリバリのエンジニア？";
+$bot->replyMessage($event->getReplyToken(),
+  (new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder())
+    ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message))
+    ->add(new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1, 114))
+);
 
-replyTextMessage($bot, $event->getReplyToken(), $message);
+// replyTextMessage($bot, $event->getReplyToken(), $message);
 // replyImageMessage($bot, $event->getReplyToken(), "https://" . $_SERVER["HTTP_HOST"] . "/imgs/original.jpg", "https://" . $_SERVER["HTTP_HOST"] . "/imgs/preview.jpg");
 // replyLocationMessage($bot, $event->getReplyToken(), "LINE", "東京都渋谷区渋谷2-21-1 ヒカリエ27階", 35.659025, 139.703473);
 // replyStickerMessage($bot, $event->getReplyToken(), 1, 1);
