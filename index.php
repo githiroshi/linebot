8b3386bd36dbd7aa5d60bfa9ea724fb4eb315c31
githiroshi <githiroshi@outlook.com>
2017-01-27 22:05:28 +0900

    rwar

--- index.php
+++ index.php
@@ -41,8 +41,7 @@ foreach ($events as $event) {
 //     ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message))
 //     ->add(new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1, 114))
 // );
-
-replyTextMessage($bot, $event->getReplyToken(), $event->getText());
+replyTextMessage($bot, $event->getReplyToken(), "test");
 // replyImageMessage($bot, $event->getReplyToken(), "https://" . $_SERVER["HTTP_HOST"] . "/imgs/original.jpg", "https://" . $_SERVER["HTTP_HOST"] . "/imgs/preview.jpg");
 // replyLocationMessage($bot, $event->getReplyToken(), "LINE", "東京都渋谷区渋谷2-21-1 ヒカリエ27階", 35.659025, 139.703473);
 // replyStickerMessage($bot, $event->getReplyToken(), 1, 1);
